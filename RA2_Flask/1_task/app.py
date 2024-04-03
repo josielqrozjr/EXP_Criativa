e13355dfc6effc9b580096f7aca7066f2ecabb84
Queiroz Jr <josielqueirozjr@icloud.com>
2024-04-03 18:52:47 -0300

    update app.py

---
+++ RA2_Flask/1_task/app.py
@@ -0,0 +1,48 @@
+# app.py
+from flask import Flask
+
+app= Flask(__name__)
+## __name__ is the application name
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return """
+    <html>
+        <head>
+            <title>Home</title>
+        </head>
+        <body>
+            <br>
+            <h1>Página incial</h1>
+            <hr>
+            <p><a href="/">Clique aqui para voltar a sua casa!</a></p>
+        </body>
+    </html>
+"""
+
+
+@app.route('/sensors')
+def sensors():
+    return """
+    <html>
+        <head>
+            <title>Minha Casa</title>
+        </head>
+        <body>
+            <br>
+            <h1>Sensores</h1>
+            <ul>
+                <li>Sensor de Umidade</li>
+                <li>Sensor de Temperatura</li>
+                <li>Sensor de Luminosidade</li>
+            </ul>
+            <hr>
+            <p><a href="/">Clique aqui para voltar a página inicial!</a></p>
+        </body>
+    </html>
+"""
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)

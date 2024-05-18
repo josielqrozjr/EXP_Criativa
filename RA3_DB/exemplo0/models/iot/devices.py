93cb3da38aef95c22a6c9fa5bbbce7c2cc4b0e05
Queiroz Jr <josielqueirozjr@icloud.com>
2024-05-18 14:25:15 -0300

    Update

--- RA3_DB/exemplo0/models/iot/devices.py
+++ RA3_DB/exemplo0/models/iot/devices.py
@@ -3,9 +3,9 @@ from models.db import db
 class Device(db.Model):
     __tablename__ = 'devices'
     id= db.Column('id', db.Integer, primary_key=True)
-    name= db.Column(db.String(50))
-    brand= db.Column(db.String(50))
-    model= db.Column(db.String(50))
+    name= db.Column(db.String(100))
+    brand= db.Column(db.String(100))
+    gender= db.Column(db.String(50))
     is_active= db.Column(db.Boolean, nullable= False, default= False)
     sensors = db.relationship('Sensor', backref='devices', lazy=True)
     actuator = db.relationship('Actuator', backref='devices', lazy=True)
